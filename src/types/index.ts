8a51d37d630f78f4539adebf62daa9fc91229e7a
Lorelvis Santos <santoslorelvis@gmail.com>
2026-02-21 08:07:53 -0400

    feat: implement activity reducer and initial state for activity management

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,10 @@
 export type Category = {
-    id: number,
-    name: string
+    id: number;
+    name: string;
 }
 
 export type Activity = {
+  id: string;
   category: number;
   name: string;
   calories: number;
